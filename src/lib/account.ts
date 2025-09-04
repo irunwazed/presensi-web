eeb307cd4ce99cc3596cd89c363e67b1f921dd6f
aka <irunwazed@gmail.com>
2025-09-04 16:11:41 +0700

    add acoount

--- src/lib/account.ts
+++ src/lib/account.ts
@@ -35,6 +35,28 @@ export const getAccount = (username: string):Account|null => {
             phModel: "22021211RG",
             phProduct: "munch_id",
         },
+        {
+            name:"SURIPTO, S.Kom.",
+            username: "199209242023211016",
+            password: "5URIPT0@bkn2024",
+            token: "7na6u3bvc8ei77fgdh7u2ahg0j169fjk",
+            phId: "2ce92207806d77a8",
+            phType: "GSM",
+            phManufacturer: "samsung",
+            phModel: "SM-M325FV",
+            phProduct: "m32xx",
+        },
+        {
+            name:"DANANG TRISDIANA PUTRA, S.Kom.",
+            username: "199804112024211004",
+            password: "Admin54321.Ayunku",
+            token: "7h4gsbcatf1v84ul5aalsp2qk17rpauc",
+            phId: "97644af1e96963b5",
+            phType: "GSM",
+            phManufacturer: "Xiaomi",
+            phModel: "22071212AG",
+            phProduct: "plato_id",
+        },
     ]
 
     for(let i = 0; i < accounts.length; i++){
